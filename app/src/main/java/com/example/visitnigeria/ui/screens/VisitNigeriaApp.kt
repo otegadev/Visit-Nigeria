0a64ae1bf603dd7dd68b4d6a506588475501cd39
Promise <103558556+otegadev@users.noreply.github.com>
2024-12-12 07:57:28 +0100

    value changes

--- app/src/main/java/com/example/visitnigeria/ui/screens/VisitNigeriaApp.kt
+++ app/src/main/java/com/example/visitnigeria/ui/screens/VisitNigeriaApp.kt
@@ -3,11 +3,9 @@ package com.example.visitnigeria.ui.screens
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -28,7 +26,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -81,13 +78,7 @@ fun VisitNigeriaApp(
                 enterTransition = {
                     slideIntoContainer(
                         towards = AnimatedContentTransitionScope.SlideDirection.Start,
-                        animationSpec = tween(600, easing = LinearEasing)
-                    )
-                },
-                exitTransition = {
-                    slideOutOfContainer(
-                        towards = AnimatedContentTransitionScope.SlideDirection.End,
-                        animationSpec = tween(600, easing = LinearEasing)
+                        animationSpec = tween(300, easing = LinearEasing)
                     )
                 }
             ) {
@@ -105,13 +96,13 @@ fun VisitNigeriaApp(
                 enterTransition = {
                     slideIntoContainer(
                         towards = AnimatedContentTransitionScope.SlideDirection.Start,
-                        animationSpec = tween(600, easing = LinearEasing)
+                        animationSpec = tween(300, easing = LinearEasing)
                     )
                 },
                 exitTransition = {
                     slideOutOfContainer(
                         towards = AnimatedContentTransitionScope.SlideDirection.End,
-                        animationSpec = tween(600, easing = LinearEasing)
+                        animationSpec = tween(300, easing = EaseOut)
                     )
                 }
             ) {
@@ -130,13 +121,13 @@ fun VisitNigeriaApp(
                 enterTransition = {
                     slideIntoContainer(
                         towards = AnimatedContentTransitionScope.SlideDirection.Start,
-                        animationSpec = tween(600, easing = LinearEasing)
+                        animationSpec = tween(300, easing = LinearEasing)
                     )
                 },
                 exitTransition = {
                     slideOutOfContainer(
                         towards = AnimatedContentTransitionScope.SlideDirection.End,
-                        animationSpec = tween(600, easing = LinearEasing)
+                        animationSpec = tween(300, easing = EaseOut)
                     )
                 }
             ) {
